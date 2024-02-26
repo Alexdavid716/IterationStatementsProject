9d390d686e18c6bbe1cf64d15df6085706109126
Alex David <alexdavid716@gmail.com>
2024-02-25 22:05:57 -0500

    lessonsExercise2

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -12,30 +12,37 @@ namespace IterationStatements
 
                 //Create a List called "numbers" - DONE!
                 var numbers = new List<int>();
-            
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
 
-
+            var num = 0;
 
             // Create a do-while loop and use the template below:
 
+            do
+            {
+                num++;
+                numbers.Add(num);
+
+            } while (num < 200);
             {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
                     // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                numbers.Add(num);
 
             } // <---- While your variable is less than 100
 
 
 
             // Create a while loop
-            // <--- While num is less than 200
+            while(num < 200)// <--- While num is less than 200
             {
                 // Increment num by 1
+                num++;
                 // Then add num to the collection numbers
                     //HINT: copy how this was done in the do while loop
 
@@ -48,7 +55,7 @@ namespace IterationStatements
 
             // Create a foreach loop using the collection - numbers
                 //In the scope of the foreach loop, print each number in numbers
-             
+            
 
 
             Console.WriteLine("");
@@ -62,7 +69,7 @@ namespace IterationStatements
                 // Decrement i by 1
 
             //start for loop here
-            {
+            {for(int i = 199; i >= 0; i--)
                 // place numbers[i] inside of the Console.WriteLine() method
             }
 
